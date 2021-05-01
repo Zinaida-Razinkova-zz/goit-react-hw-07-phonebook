0b8ef5c3607cd01a5a44ab2faadc2712c42a96f1
Zinaida Razinkova <razinkova.zina@gmail.com>
2021-05-01 14:57:26 +0300

    Добавляет ДЗ

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -3,7 +3,7 @@ import { createReducer } from "@reduxjs/toolkit";
 import actions from "./actions";
 
 const items = createReducer([], {
-  // [actions.fetchContactSuccess]: (_, action) => action.payload,
+  [actions.fetchContactSuccess]: (_, action) => action.payload,
   [actions.addContactSuccess]: (state, action) => [...state, action.payload],
   [actions.deleteContactSuccess]: (state, action) =>
     state.filter((contact) => contact.id !== action.payload),
